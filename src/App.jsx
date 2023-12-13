afc055aba0d7e34ef96bdbc9f67477d3c55bcb15
sAeed <saeed@sAeeds-MacBook-Air.local>
2023-12-13 15:10:47 +0330

    Add Page404 component to erorElement in router of App component

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,15 @@ import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import Home from './pages/Home'
 import ProductDetails from './pages/ProductDetails'
 import Layout from './components/Layout'
+import Page404 from './components/Page404'
 
 function App() {
     const router = createBrowserRouter([
         {
             // parent route component
             element: <Layout />,
+            // your custom routing error component
+            errorElement: <Page404 />,
             // child route components
             children: [
                 {
