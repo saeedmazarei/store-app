c2633199d86ca04d08a4e0f897cf7abd8898cb41
sAeed <saeed@sAeeds-MacBook-Air.local>
2023-12-13 18:07:22 +0330

    Add a onClick props to Button component

--- src/components/shared/Button.jsx
+++ src/components/shared/Button.jsx
@@ -1,13 +1,18 @@
 import PropTypes from 'prop-types'
 import styles from './Button.module.scss'
 
-function Button({ children, style }) {
-    return <button style={style} className={styles.button}>{children}</button>
+function Button({ children, style, onClick }) {
+    return (
+        <button style={style} className={styles.button} onClick={onClick}>
+            {children}
+        </button>
+    )
 }
 
 Button.propTypes = {
     children: PropTypes.node.isRequired,
-    style: PropTypes.object.isRequired
+    style: PropTypes.object.isRequired,
+    onClick: PropTypes.func.isRequired
 }
 
 export default Button
