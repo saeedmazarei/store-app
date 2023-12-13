2057db249cc8a23123d8db08cea8a511e71903ee
sAeed <saeed@sAeeds-MacBook-Air.local>
2023-12-13 16:42:29 +0330

    Add sass to project and Create Card component for wraping data and add some css to it

--- src/components/shared/Card.jsx
+++ src/components/shared/Card.jsx
@@ -1,5 +1,13 @@
+import PropTypes from 'prop-types'
+
+import styles from './Card.module.css'
+
 function Card({ children }) {
-    return <div>{children}</div>
+    return <div className={styles.card}>{children}</div>
+}
+
+Card.propTypes = {
+    children: PropTypes.node.isRequired,
 }
 
 export default Card
