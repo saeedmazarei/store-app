22f6fbeb591b7744edddc7802a38690d62dfd992
sAeed <saeed@sAeeds-MacBook-Air.local>
2023-12-13 17:14:43 +0330

    Add style to Header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,3 +1,9 @@
-function Header() {}
+import Card from './shared/Card'
+
+function Header() {
+    return <Card style={{ height: '120px', borderTopLeftRadius: '0', borderTopRightRadius: '0' }}>
+        
+    </Card>
+}
 
 export default Header
