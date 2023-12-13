b2d1e04aede91a8daf2861ad0934a394555f46c7
sAeed <saeed@sAeeds-MacBook-Air.local>
2023-12-13 14:41:48 +0330

    Add Header, Main and Footer components to Layout component

--- src/components/Layouts.jsx
+++ src/components/Layouts.jsx
@@ -1,3 +1,18 @@
-function Layouts() {}
+import { Outlet } from 'react-router-dom'
+import Header from './Header'
+import Main from './Main'
+import Footer from './Footer'
+
+function Layouts() {
+    return (
+        <>
+            <Header />
+            <Main>
+                <Outlet />
+            </Main>
+            <Footer />
+        </>
+    )
+}
 
 export default Layouts
