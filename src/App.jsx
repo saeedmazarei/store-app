8ecd9ef6e7fbef911443e885abef9e01d5b15625
sAeed <saeed@sAeeds-MacBook-Air.local>
2023-12-13 14:53:43 +0330

    Setup routes in App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,28 @@
-
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import Home from './pages/Home'
+import ProductDetails from './pages/ProductDetails'
+import Layouts from './components/Layouts'
 
 function App() {
-
-  return (
-    <>
-      test
-    </>
-  )
+    const router = createBrowserRouter([
+        {
+            // parent route component
+            element: <Layouts />,
+            // child route components
+            children: [
+                {
+                    path: '/',
+                    element: <Home />,
+                },
+                // other pages....
+                {
+                    path: '/about',
+                    element: <ProductDetails />,
+                },
+            ],
+        },
+    ])
+    return <RouterProvider router={router} />
 }
 
 export default App
