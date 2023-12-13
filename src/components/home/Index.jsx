2d2f756f12bf122e001657e1a2171d49b34d6328
sAeed <saeed@sAeeds-MacBook-Air.local>
2023-12-13 15:52:48 +0330

    Add Index component to components/home directory for main section of home page

---
+++ src/components/home/Index.jsx
@@ -0,0 +1,5 @@
+function Index() {
+    return <h1>main</h1>
+}
+
+export default Index
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,3 +1,7 @@
-function Home() {}
+import Index from "../components/home/Index"
+
+function Home() {
+    return <Index />
+}
 
 export default Home
