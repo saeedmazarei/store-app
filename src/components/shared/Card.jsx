c0b1beac7ad7001b55bdc85fcdf4405d8024b1ff
sAeed <saeed@sAeeds-MacBook-Air.local>
2023-12-14 15:58:30 +0330

    Make header responsive for all size

--- src/components/shared/Card.jsx
+++ src/components/shared/Card.jsx
@@ -2,13 +2,14 @@ import PropTypes from 'prop-types'
 
 import styles from './Card.module.scss'
 
-function Card({ children, style }) {
-    return <div className={styles.card} style={style}>{children}</div>
+function Card({ children, style, className }) {
+    return <div className={`${styles.card} ${className}`} style={style}>{children}</div>
 }
 
 Card.propTypes = {
     children: PropTypes.node.isRequired,
-    style: PropTypes.object.isRequired
+    style: PropTypes.object.isRequired,
+    className: PropTypes.string.isRequired
 }
 
 export default Card
