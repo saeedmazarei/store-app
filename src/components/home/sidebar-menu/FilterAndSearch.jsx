27db3f2fbe298aba03ea183c8a6c6fb322627680
sAeed <saeed@sAeeds-MacBook-Air.local>
2023-12-15 19:07:26 +0330

    Add filter and search box ui

---
+++ src/components/home/sidebar-menu/FilterAndSearch.jsx
@@ -0,0 +1,24 @@
+import { useTranslation } from "react-i18next"
+import { CiSearch } from "react-icons/ci"
+import { RxCross2 } from "react-icons/rx"
+
+import Card from "../../shared/Card"
+import Button from "../../shared/Button"
+import styles from './SideBarMenu.module.scss'
+
+function FilterAndSearch() {
+    const { t } = useTranslation()
+    return <Card style={{width: '100%', height: '160px'}} className={styles.card}>
+        <div className={styles["text-container"]}>
+            <span>{t('home.filterAndSearch')}</span>
+        </div>
+        <div className={styles["search-box"]}>
+            <CiSearch style={{color: '#E20054'}} />
+            <input type="text" />
+            <RxCross2 />
+        </div>
+        <Button className={styles.button}>{t('home.search')}</Button>
+    </Card>
+}
+
+export default FilterAndSearch
--- src/components/home/sidebar-menu/SideBarMenu.jsx
+++ src/components/home/sidebar-menu/SideBarMenu.jsx
@@ -1,9 +1,10 @@
 import Card from '../../shared/Card'
+import FilterAndSearch from './FilterAndSearch'
 import styles from './SideBarMenu.module.scss'
 
 function SideBarMenu() {
     return <div className={styles['side-bar-container']}>
-        <Card style={{width: '100%', height: '160px'}} />
+        <FilterAndSearch />
         <Card style={{width: '100%', height: '200px'}} />
         <Card style={{width: '100%', height: '180px'}} />
     </div>
