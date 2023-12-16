e757ab2b2243ff7dab47a69626d2572e4f16f69f
sAeed <saeed@sAeeds-MacBook-Air.local>
2023-12-17 01:06:08 +0330

    Refactor Description card ui for fixing bug in mobile size

--- src/components/product-details/description/DescrptionCard.jsx
+++ src/components/product-details/description/DescrptionCard.jsx
@@ -10,50 +10,36 @@ function DescriptionCard({ productDetails }) {
         <Card className={styles['description-container']}>
             <p>{t('productDetails.technicalSpecifications')}</p>
             <div className={styles['table-container']}>
-                <div className={styles['right-boxes-container']}>
-                    <Card className={`${styles['technical-title']} ${styles['small-height']}`}>
-                        <span>{t('productDetails.price')}</span>
-                    </Card>
-                    <Card className={`${styles['technical-title']} ${styles['tall-height']}`}>
-                        <span>{t('productDetails.description')}</span>
-                    </Card>
-                    <Card className={`${styles['technical-title']} ${styles['small-height']}`}>
-                        <span>{t('home.grouping')}</span>
-                    </Card>
-                    <Card className={`${styles['technical-title']} ${styles['small-height']}`}>
-                        <span>{t('productDetails.rank')}</span>
-                    </Card>
-                    <Card className={`${styles['technical-title']} ${styles['small-height']}`}>
+                <Card className={`${styles['technical-title']} ${styles['small-height']}`}>
+                    <span>{t('productDetails.price')}</span>
+                </Card>
+                <Card className={`${styles['technical-description']} ${styles['small-height']}`}>
+                    <span>{productDetails?.price}</span>
+                </Card>
+                <Card className={`${styles['technical-title']} ${styles['tall-height']}`}>
+                    <span>{t('productDetails.description')}</span>
+                </Card>
+                <Card className={`${styles['technical-description']} ${styles['tall-height']}`}>
+                    <span>{productDetails?.description}</span>
+                </Card>
+                <Card className={`${styles['technical-title']} ${styles['small-height']}`}>
+                    <span>{t('home.grouping')}</span>
+                </Card>
+                <Card className={`${styles['technical-description']} ${styles['small-height']}`}>
+                    <span>{productDetails?.category}</span>
+                </Card>
+                <Card className={`${styles['technical-title']} ${styles['small-height']}`}>
+                    <span>{t('productDetails.rank')}</span>
+                </Card>
+                <Card className={`${styles['technical-description']} ${styles['small-height']}`}>
+                    <span>{productDetails?.rating.rate}</span>
+                </Card>
+                <Card className={`${styles['technical-title']} ${styles['small-height']}`}>
                         <span>{t('productDetails.number')}</span>
                     </Card>
-                </div>
-                <div className={styles['left-boxes-container']}>
-                    <Card
-                        className={`${styles['technical-description']} ${styles['small-height']}`}
-                    >
-                        <span>{productDetails?.price}</span>
-                    </Card>
-                    <Card
-                        className={`${styles['technical-description']} ${styles['tall-height']}`}
-                    >
-                        <span>{productDetails?.description}</span>
-                    </Card>
-                    <Card
-                        className={`${styles['technical-description']} ${styles['small-height']}`}
-                    >
-                        <span>{productDetails?.category}</span>
-                    </Card>
-                    <Card
-                        className={`${styles['technical-description']} ${styles['small-height']}`}
-                    >
-                        <span>{productDetails?.rating.rate}</span>
-                    </Card>
-                    <Card
-                        className={`${styles['technical-description']} ${styles['small-height']}`}
-                    >
-                        <span>{productDetails?.rating.count}</span>
-                    </Card>
-                </div>
+                <Card className={`${styles['technical-description']} ${styles['small-height']}`}>
+                    <span>{productDetails?.rating.count}</span>
+                </Card>
             </div>
         </Card>
     )
