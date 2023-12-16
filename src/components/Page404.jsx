2a7b3306d61aa0f4ba177c7700f171be2b1c0eaf
sAeed <saeed@sAeeds-MacBook-Air.local>
2023-12-16 22:15:32 +0330

    Complete Page404 component

--- src/components/Page404.jsx
+++ src/components/Page404.jsx
@@ -1,3 +1,35 @@
-function Page404() {}
+import { useTranslation } from 'react-i18next'
+
+import Footer from './footer/Footer'
+import Header from './header/Header'
+import Button from './shared/button/Button'
+import { Link } from 'react-router-dom'
+
+function Page404() {
+    const { t } = useTranslation()
+    return (
+        <>
+            <Header />
+            <main>
+                <div
+                    style={{
+                        display: 'flex',
+                        flexDirection: 'column',
+                        height: '500px',
+                        justifyContent: 'center',
+                        alignItems: 'center',
+                    }}
+                >
+                    <h1>{t('notFoundPage.404')}</h1>
+                    <h2>{t('notFoundPage.notFound')}</h2>
+                    <Link to="/">
+                        <Button style={{marginTop: '20px', borderRadius: '16px', padding: '10px', cursor: 'pointer'}}>{t('notFoundPage.backToHome')}</Button>
+                    </Link>
+                </div>
+            </main>
+            <Footer />
+        </>
+    )
+}
 
 export default Page404
