395b03553b0109b596a2ba0617cde55f0da97ee5
sAeed <saeed@sAeeds-MacBook-Air.local>
2023-12-16 17:08:46 +0330

    Add slice to redux

--- src/store/index.js
+++ src/store/index.js
@@ -1,7 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit"
 
+import productsListReducer from './slices/products'
+
 const store = configureStore({
-    reducer: {}
+    reducer: {
+        productsList: productsListReducer
+    }
 })
 
 export default store
---
+++ src/store/slices/products.js
@@ -0,0 +1,23 @@
+import { createSlice } from "@reduxjs/toolkit"
+
+const initialState = {
+    productsList: null,
+}
+
+const productsSlice = createSlice({
+    name: 'products',
+    initialState,
+    reducers: {
+        setProductsList: (state, action) => {
+            state.productsList = action.payload
+        }
+    }
+})
+
+export const { setProductsList } = productsSlice.actions
+
+export default productsSlice.reducer
+
+// selectors
+
+export const selectProductsList = (state) => state.productsList.productsList
