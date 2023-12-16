aea4835283d49af60cee10f574dcfca0b6c6a610
sAeed <saeed@sAeeds-MacBook-Air.local>
2023-12-16 14:03:54 +0330

    Add functionality to get product details in Index component of product-details directory

--- src/components/product-details/Index.jsx
+++ src/components/product-details/Index.jsx
@@ -1,4 +1,28 @@
+import { useEffect, useState } from 'react'
+
+import { useParams } from 'react-router-dom'
+
+import { getSingleProduct } from '../../services/apis'
+
 function Index() {
+    const [productDetails, setProductDetails] = useState()
+    const [loading, setLoading] = useState(false)
+    const { id } = useParams()
+
+    useEffect(() => {
+        async function getProductDetails() {
+            try {
+                setLoading(true)
+                const response = await getSingleProduct(id)
+                setProductDetails(response.data)
+                setLoading(false)
+            } catch {
+                setLoading(false)
+            }
+        }
+        getProductDetails()
+    }, [id])
+
     return <h1>Product details</h1>
 }
 
--- src/services/apis.js
+++ src/services/apis.js
@@ -5,3 +5,5 @@ export const getAllProduct = () => api.get('products')
 export const sortProduct = (method) => api.get(`products?sort=${method}`)
 
 export const categorisedProduct = (category) => api.get(`products/category/${category}`)
+
+export const getSingleProduct = (id) => api.get(`products/${id}`)
