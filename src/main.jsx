8fb7434bebe11a07ec183b58c28377c7347d9f38
sAeed <saeed@sAeeds-MacBook-Air.local>
2023-12-16 17:13:43 +0330

    Wrap hole app by redux provider

--- src/main.jsx
+++ src/main.jsx
@@ -5,11 +5,14 @@ import { I18nextProvider } from 'react-i18next'
 import i18n from './i18n/index.js'
 
 import App from './App.jsx'
+import ReduxStoreProvider from './providers/ReduxStoreProvider.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
-        <I18nextProvider i18n={i18n}>
-            <App />
-        </I18nextProvider>
+        <ReduxStoreProvider>
+            <I18nextProvider i18n={i18n}>
+                <App />
+            </I18nextProvider>
+        </ReduxStoreProvider>
     </React.StrictMode>,
 )
