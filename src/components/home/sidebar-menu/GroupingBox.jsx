80135ba43d87b202a8644d86cf73bb6732d3155a
sAeed <saeed@sAeeds-MacBook-Air.local>
2023-12-16 19:02:01 +0330

    Add functionality for grouping component to let user to select specific category and show it the result

--- src/components/home/sidebar-menu/GroupingBox.jsx
+++ src/components/home/sidebar-menu/GroupingBox.jsx
@@ -1,8 +1,10 @@
 import { useEffect, useState } from 'react'
 
+import { useDispatch, useSelector } from 'react-redux'
 import { useTranslation } from 'react-i18next'
 import { IoIosArrowDown } from 'react-icons/io'
 
+import { setProductsList, selectProductsList } from '../../../store/slices/products'
 import Card from '../../shared/card/Card'
 import styles from './SideBarMenu.module.scss'
 import { categorisedProduct } from '../../../services/apis'
@@ -13,7 +15,14 @@ function Groupingbox() {
         "men's clothing": false,
         electronics: false,
     })
+    const dispatch = useDispatch()
+    const productsList = useSelector(selectProductsList)
     const { t } = useTranslation()
+
+    const categoryCount = productsList?.reduce((acc, item) => {
+        acc[item.category] = (acc[item.category] || 0) + 1
+        return acc
+    }, {})
 
     const handleCheckboxChange = (itemName) => {
         setCheckedItems({
@@ -23,16 +32,23 @@ function Groupingbox() {
     }
 
     useEffect(() => {
-        const selectedCategories = Object.keys(checkedItems).filter((key) => checkedItems[key])
-        selectedCategories.forEach(async (category) => {
+        const fetchDataForCategories = async () => {
             try {
-                const response = await categorisedProduct(category)
-                console.log(response.data)
+                const selectedCategories = Object.keys(checkedItems).filter(
+                    (key) => checkedItems[key],
+                )
+                const newProductsList = []
+                for (const category of selectedCategories) {
+                    const response = await categorisedProduct(category)
+                    newProductsList.push(...response.data)
+                }
+                dispatch(setProductsList(newProductsList))
             } catch (error) {
-                console.error('Error while fetching category:', category, error)
+                console.error('Error while fetching categories:', error)
             }
-        })
-    }, [checkedItems])
+        }
+        fetchDataForCategories()
+    }, [checkedItems, dispatch])
 
     return (
         <Card style={{ width: '100%', height: '180px' }} className={styles.card}>
@@ -50,7 +66,7 @@ function Groupingbox() {
                             checked={checkedItems.jewelery}
                             onChange={() => handleCheckboxChange('jewelery')}
                         />
-                        <span>{t('home.building')}</span>
+                        <span>{t('home.jewelery')}</span>
                     </label>
                     <label>
                         <input
@@ -58,7 +74,7 @@ function Groupingbox() {
                             checked={checkedItems["men's clothing"]}
                             onChange={() => handleCheckboxChange("men's clothing")}
                         />
-                        <span>{t('home.health')}</span>
+                        <span>{t('home.mensClothing')}</span>
                     </label>
                     <label>
                         <input
@@ -66,13 +82,13 @@ function Groupingbox() {
                             checked={checkedItems.electronics}
                             onChange={() => handleCheckboxChange('electronics')}
                         />
-                        <span>{t('home.factory')}</span>
+                        <span>{t('home.electronics')}</span>
                     </label>
                 </div>
                 <div className={styles['number-box-container']}>
-                    <div className={styles['number-box']}>12</div>
-                    <div className={styles['number-box']}>12</div>
-                    <div className={styles['number-box']}>12</div>
+                    <div className={styles['number-box']}>{categoryCount?.jewelery}</div>
+                    <div className={styles['number-box']}>{categoryCount?.["men's clothing"]}</div>
+                    <div className={styles['number-box']}>{categoryCount?.electronics}</div>
                 </div>
             </div>
         </Card>
